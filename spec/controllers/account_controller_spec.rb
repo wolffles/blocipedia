c8a9246b64ec52fb2d8ad3f1be61733bceff137b
wolffles <tofukillzstevent@live.com>
2017-08-31 18:05:49 -0700

    added option to change from private to public in views, updated the controllers to accept the parameters, and made it so downgrading with publicize the private wikiz.

--- spec/controllers/account_controller_spec.rb
+++ spec/controllers/account_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe AccountController, type: :controller do
+  before(:each) do
+    @my_user = FactoryGirl.create(:user)
+    @my_user.update_attribute(:role, :premium)
+    sign_in @my_user
+  end
 
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+describe "GET downgrade" do
+  it "returns http success" do
+    get :downgrade
+    expect(response).to have_http_status(:success)
+  end
+
+  it "changes a premium user to standard" do
+    post :downgrading, params: {current_user: @my_user}
+    expect(@my_user.role).to eq "standard"
+  end
+end
 end
