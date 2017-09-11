e05bf45afd5a26d73069e79fee9f2ce531453e3c
wolffles <tofukillzstevent@live.com>
2017-09-10 23:59:15 -0700

    there was an issue, where the id params don't get set to @emails_strings.

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,18 +1,29 @@
 class CollaboratorsController < ApplicationController
 
-  def create
-      @emails_string = collaborator_params[:id]
-      # your turn to turns the email strings into Collaborators
+  def new
+    @emails_strings = collaborator_params[:id]
+    @array_emails = @emails_strings.split
+  end
 
-      @collaborator = Collaborator.new(collaborator_params)
-      if @collaborator.save
-        flash[:notice] = "\"#{@collaborator.user.email}\" has been successfully added.Yay!"
-        redirect_to Wiki.find_by(params[:collaborator][:wiki_id])
-      else
-        flash.now[:alert] = "Bummer!There was an error adding the collaborator \"#{@collaborator.user.email}\" :( .Please try again."
-        redirect_to Wiki.find_by(params[:wiki_id])
-      end
-    end
+  def create
+    @emails_string = collaborator_params[:id]
+    @array_emails = @emails_strings.split
+    flash.now[:alert]= "these are your #{@emails_string}"
+     @invalid, @valid = [], []
+     @array_emails.each{ |email|
+       collaborator_id = User.where(email: email).pluck(:id)
+       if collaborator_id.empty?
+         flash.now[:alert] = "Bummer! There was an error adding the collaborator \"#{email}\" :( .Please try again."
+       else
+         @collaborator = Collaborator.create(user_id: collaborator_id, wiki_id: wiki_id)
+         if @collaborator.save
+           flash[:notice] = "\"#{@collaborator.user.email}\" has been successfully added.Yay!"
+         else
+           flash.now[:alert] = "Bummer!There was an error adding the collaborator \"#{@collaborator.user.email}\" :( .Please try again."
+         end
+       end
+     }
+  end
 
   def destroy
     @collaborator = Collaborator.find(params[:id])
@@ -32,17 +43,17 @@ private
     params.require(:collaborator).permit(:wiki_id, :user_id, :id)
   end
 
-  # def add_collaborator(text, wiki_id)
-  #   invalid, valid = [],[]
-  #   %w(text).each { |email|
-  #     collaborator_id = User.where(email: email).pluck(:id)
-  #     if User.where(email: email).empty?
-  #       invalid << email
-  #     else
-  #       valid << email
-  #       Collaborator.create(user_id: collaborator_id, wiki_id: wiki_id)
-  #     end
-  #   }
-  # end
+  def add_collaborator(text, wiki_id)
+    invalid, valid = [],[]
+    %w(text).each { |email|
+      collaborator_id = User.where(email: email).pluck(:id)
+      if User.where(email: email).empty?
+        invalid << email
+      else
+        valid << email
+        Collaborator.create(user_id: collaborator_id, wiki_id: wiki_id)
+      end
+    }
+  end
 
   end
