6c1a6fdfa428c4b66f1df93ee63edf1e1d13bfbc
woffles <wolfgang.truong@gmail.com>
2017-10-25 18:20:34 -0700

    testing dataworkz

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-var dataworkz= {}
-dataworkz.report = function(eventName){
- var event = {event: {name:eventName}};
- var request = new XMLHttpRequest();
- request.open("POST", "http://localhost:3000/api/events", true)
- request.setRequestHeader('Content-Type', 'application/json');
- request.send(JSON.stringify(event));
-};
